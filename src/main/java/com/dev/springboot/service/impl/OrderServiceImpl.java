67ffc0e740ac77a5381e292a1d1c887fa2efe7ff
Bachir <bafosth@yahoo.fr>
2024-02-08 09:54:34 -0500

    added script in OrderController and OrderServiceImpl

--- src/main/java/com/dev/springboot/service/impl/OrderServiceImpl.java
+++ src/main/java/com/dev/springboot/service/impl/OrderServiceImpl.java
@@ -4,10 +4,14 @@ import com.dev.springboot.dto.OrderRequest;
 import com.dev.springboot.dto.OrderResponse;
 import com.dev.springboot.entity.Order;
 import com.dev.springboot.entity.Payment;
+import com.dev.springboot.exception.PaymentException;
 import com.dev.springboot.repository.OrderRepository;
 import com.dev.springboot.repository.PaymentRepository;
 import com.dev.springboot.service.OrderService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -21,17 +25,33 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional // To avoid or manage data inconsistency
     public OrderResponse placeOrder(OrderRequest orderRequest) {
 
+        // This method has 2 functions:
+
+        // 1 - Save the order details
         Order order = orderRequest.getOrder();
         order.setStatus("IN PROGRESS");
+        order.setOrderTrackingNumber(UUID.randomUUID().toString());
         orderRepository.save(order);
 
         Payment payment = orderRequest.getPayment();
-        
 
+        if(!payment.getType().equals("DEBIT")) {
+            throw new PaymentException("Payment type is not supported");
+        }
 
-        return null;
+        // 2 - Save the payment details
+        payment.setOrderId(order.getId());
+        paymentRepository.save(payment);
+
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setOrderTrackingNumber(order.getOrderTrackingNumber());
+        orderResponse.setStatus(order.getStatus());
+        orderResponse.setMessage("SUCCESS");
+
+        return orderResponse;
     }
 }
 
